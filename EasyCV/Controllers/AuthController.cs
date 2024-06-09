5631bf6f924b31e4817ff71274de3726bbe159b2
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-06-09 23:12:08 +0400

    Add project files.

---
+++ EasyCV/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCV.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+         public IActionResult Profile()
+        {
+            return View();  
+        }
+    }
+}
