09bc529cac0034265fdbe10babd0c419f2c56475
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-09-11 13:20:16 -0700

    setup Swagger.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express';
 //import bodyParser from 'body-parser';
 import routes from './src/Routes/Main.js';
 import sequelize from './src/Config/Database.js';
+import swaggerDocs from './src/Config/Swagger.js';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,11 +12,16 @@ app.use(express.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
+swaggerDocs(app);
 
-sequelize.sync({ alter: true }).then(() => {
-    app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
-    });
-}).catch((error) => {
-    console.error('Unable to connect to the database:', error);
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
+
+// sequelize.sync({ alter: true }).then(() => {
+//     app.listen(port, () => {
+//         console.log(`Server is running at http://localhost:${port}`);
+//     });
+// }).catch((error) => {
+//     console.error('Unable to connect to the database:', error);
+// });
