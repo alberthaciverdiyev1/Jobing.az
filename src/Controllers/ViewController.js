363b076257f47a5ff7bfefe5f590358952a6a01c
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-10-09 12:10:31 -0700

    front routes

---
+++ src/Controllers/ViewController.js
@@ -0,0 +1,9 @@
+const ViewController = {
+    home: async (req, res) => {
+        res.render("../views/home/index.ejs");
+    }, auth: async (req, res) => {
+        res.render("../views/home/index.ejs");
+    },
+};
+
+export default ViewController;
--- src/Routes/Main.js
+++ src/Routes/Main.js
@@ -5,48 +5,52 @@ import categoryController from '../Controllers/CategoryController.js';
 import jobDataController from '../Controllers/JobDataController.js';
 import siteController from '../Controllers/siteController.js';
 import scrapeController from '../Controllers/scrapeController.js';
+import viewController from "../Controllers/ViewController.js";
 
 import validator from '../Validators/Main.js'
-import category from "../Models/Category.js";
 const router = express.Router();
 
-router.post('/users', validator.registerValidator, userController.createUser);         // CREATE
-router.get('/users', userController.getUsers);                                         // READ ALL
-router.get('/users/:id', userController.getUserById);                                  // READ ONE
-router.put('/users/:id', userController.updateUser);                                   // UPDATE
-router.delete('/users/:id', userController.deleteUser);                                // DELETE
+router.post('/api/users', validator.registerValidator, userController.createUser);         // CREATE
+router.get('/api/users', userController.getUsers);                                         // READ ALL
+router.get('/api/users/:id', userController.getUserById);                                  // READ ONE
+router.put('/api/users/:id', userController.updateUser);                                   // UPDATE
+router.delete('/api/users/:id', userController.deleteUser);                                // DELETE
 
 // CRUD operations for job sites (JobDataController)
-router.post('/jobs', jobDataController.create);                                    // CREATE
-router.get('/jobs', jobDataController.getSites);                                       // READ ALL
-router.get('/jobs/:id', jobDataController.getSiteById);                                // READ ONE
-router.put('/jobs/:id', jobDataController.updateSite);                                 // UPDATE
-router.delete('/jobs/:id', jobDataController.deleteSite);                              // DELETE
+router.post('/api/jobs', jobDataController.create);                                        // CREATE
+router.get('/api/jobs', jobDataController.getSites);                                       // READ ALL
+router.get('/api/jobs/:id', jobDataController.getSiteById);                                // READ ONE
+router.put('/api/jobs/:id', jobDataController.updateSite);                                 // UPDATE
+router.delete('/api/jobs/:id', jobDataController.deleteSite);                              // DELETE
 
 // CRUD operations for job sites (JobDataController)
-router.post('/site', validator.siteValidator, siteController.create);                  // CREATE
-router.get('/site', siteController.getAll);                                            // READ ALL
-router.get('/site/:id', siteController.findById);                                      // READ ONE
-router.put('/site/:id', validator.siteValidator, siteController.update);               // UPDATE
-router.delete('/site/:id', siteController.delete);                                     // DELETE
+router.post('/api/site', validator.siteValidator, siteController.create);                  // CREATE
+router.get('/api/site', siteController.getAll);                                            // READ ALL
+router.get('/api/site/:id', siteController.findById);                                      // READ ONE
+router.put('/api/site/:id', validator.siteValidator, siteController.update);               // UPDATE
+router.delete('/api/site/:id', siteController.delete);                                     // DELETE
 
 // CRUD operations for companies
-router.post('/companies',validator.companyValidator, companyController.create);        // CREATE
-router.get('/companies', companyController.getAll);                                    // READ ALL
-router.get('/companies/:id', companyController.findById);                              // READ ONE
-router.put('/companies/:id',validator.companyValidator, companyController.update);     // UPDATE
-router.delete('/companies/:id', companyController.delete);                             // DELETE
+router.post('/api/companies',validator.companyValidator, companyController.create);        // CREATE
+router.get('/api/companies', companyController.getAll);                                    // READ ALL
+router.get('/api/companies/:id', companyController.findById);                              // READ ONE
+router.put('/api/companies/:id',validator.companyValidator, companyController.update);     // UPDATE
+router.delete('/api/companies/:id', companyController.delete);                             // DELETE
 
 // CRUD operations for categories
-router.post('/categories', categoryController.create);        // CREATE
-router.get('/categories', categoryController.getAll);                                    // READ ALL
+router.post('/api/categories', categoryController.create);        // CREATE
+router.get('/api/categories', categoryController.getAll);                                    // READ ALL
 // router.get('/categories/:id', categoryController.findById);                              // READ ONE
 // router.put('/categories/:id',validator.companyValidator, categoryController.update);     // UPDATE
 // router.delete('/categories/:id', categoryController.delete);                             // DELETE
 
 
 // CRUD operations for scrape
-router.get('/scrape', scrapeController.getData);                                    // READ ALL
+router.get('/api/scrape', scrapeController.getData);                                    // READ ALL
 
+
+//Load Views
+router.get('/home',viewController.home);
+router.get('/auth',viewController.auth);
 
 export default router;
