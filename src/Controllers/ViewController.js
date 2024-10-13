53ac695351260bd71bc436fac64958fae59af5eb
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-10-13 10:14:36 -0700

    setup home/js/ejs/css

--- src/Controllers/ViewController.js
+++ src/Controllers/ViewController.js
@@ -1,9 +1,27 @@
 const ViewController = {
     home: async (req, res) => {
-        res.render("home/index.hbs");
-    }, 
+        const view = {
+            title: 'Home',
+            body: "home/index.ejs",
+            js: "Home.js"
+        };
+        res.render('main', view);
+    },
     auth: async (req, res) => {
-        res.render("auth/index.hbs");
+        const view = {
+            title: 'Auth',
+            body: "auth/index.ejs",
+            js: "Auth.js"
+        };
+        res.render('main', view);
+    },
+    jobs: async (req, res) => {
+        const view = {
+            title: 'Jobs',
+            body: "jobs/index.ejs",
+            js: "Jobs.js"
+        };
+        res.render('main', view);
     },
 };
 
---
+++ src/Public/Js/Home.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log("Home fully loaded and parsed");
+  });
