70967738e673bc7388afa5eeecfd48f5f5a4f4d0
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-10-27 12:50:03 -0700

    changed port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import swaggerDocs from './src/Config/Swagger.js';
 import loggerMiddleware from './src/Middlewares/Logger.js';
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PR_PORT || 3001;
 
 app.set('view engine', 'ejs');
 app.set('views', './src/Views');
