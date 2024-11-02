4897b5bd31d58291ecbabb65e5b7a37c2222898b
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-11-02 00:00:35 -0700

    contact us, global jsj

--- src/Public/Js/Global.js
+++ src/Public/Js/Global.js
@@ -1,21 +1,23 @@
-document.getElementById("menuToggle").onclick = function () {
-    document.getElementById("mobile-menu").classList.toggle("open");
-    document.getElementById("menuBackdrop").classList.toggle("open");
-};
+document.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("menuToggle").onclick = function () {
+        document.getElementById("mobile-menu").classList.toggle("open");
+        document.getElementById("menuBackdrop").classList.toggle("open");
+    };
 
-document.getElementById("menuClose").onclick = function () {
-    document.getElementById("mobile-menu").classList.remove("open");
-    document.getElementById("menuBackdrop").classList.remove("open");
-};
+    document.getElementById("menuClose").onclick = function () {
+        document.getElementById("mobile-menu").classList.remove("open");
+        document.getElementById("menuBackdrop").classList.remove("open");
+    };
 
-document.getElementById("menuBackdrop").onclick = function () {
-    document.getElementById("mobile-menu").classList.remove("open");
-    document.getElementById("menuBackdrop").classList.remove("open");
-};
-document.getElementsByClassName("fa-chevron-up").onclick = function () {
-    this.item
-}
-document.getElementById("mobile-filter-btn").onclick = function () {
-    document.getElementById("filter-section").classList.toggle("hidden");
-    document.getElementById("card-section").classList.toggle("hidden");
-};
+    document.getElementById("menuBackdrop").onclick = function () {
+        document.getElementById("mobile-menu").classList.remove("open");
+        document.getElementById("menuBackdrop").classList.remove("open");
+    };
+    document.getElementsByClassName("fa-chevron-up").onclick = function () {
+        this.item
+    }
+    document.getElementById("mobile-filter-btn").onclick = function () {
+        document.getElementById("filter-section").classList.toggle("hidden");
+        document.getElementById("card-section").classList.toggle("hidden");
+    };
+});
