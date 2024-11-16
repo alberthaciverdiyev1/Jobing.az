7b249b2c8d8ef625354cbb7565c5a8bdff471f02
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-11-16 00:50:07 -0800

    #fe8011 for buttons

--- app.js
+++ app.js
@@ -28,101 +28,101 @@ app.use('/', routes);
 
 // swaggerDocs(app);
 
-// cron.schedule('*/10 * * * *', async () => {
-// // cron.schedule('0 7-23/2 * * *', async () => {
-// //     const to = process.env.CRON_MAIL_USER;
-// //     const now = new Date();
+cron.schedule('*/10 * * * *', async () => {
+// cron.schedule('0 7-23/2 * * *', async () => {
+    const to = process.env.CRON_MAIL_USER;
+    const now = new Date();
 
-// //     const formatDate = (date) => {
-// //         return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
-// //     };
+    const formatDate = (date) => {
+        return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+    };
 
-// //     try {
-// //         let startData = {
-// //             title: "Cron started",
-// //             text: `Crone started at ${formatDate(now)}`
-// //         };
-// //         await sendEmail(startData, to);
+    try {
+        let startData = {
+            title: "Cron started",
+            text: `Crone started at ${formatDate(now)}`
+        };
+        await sendEmail(startData, to);
 
-// //         const response = await axios.post(`http://localhost:${port}/api/jobs`);
+        const response = await axios.post(`http://localhost:${port}/api/jobs`);
 
-// //         if (response.status === 200 || response.status === 201) {
-// //             let endData = {
-// //                 title: "Cron ended",
-// //                 text: `${response.data.message}`
-// //             };
-// //             await sendEmail(endData, to);
-// //         }
-// //     } catch (error) {
-// //         let errorData = {
-// //             title: "Cron ended with error",
-// //             text: `${error}`
-// //         };
-// //         await sendEmail(errorData, to);
-// //     }
-// // });
+        if (response.status === 200 || response.status === 201) {
+            let endData = {
+                title: "Cron ended",
+                text: `${response.data.message}`
+            };
+            await sendEmail(endData, to);
+        }
+    } catch (error) {
+        let errorData = {
+            title: "Cron ended with error",
+            text: `${error}`
+        };
+        await sendEmail(errorData, to);
+    }
+});
 
-// // cron.schedule('0 7-23/2 * * *', async () => {
-//     const to = process.env.CRON_MAIL_USER;
+// cron.schedule('0 7-23/2 * * *', async () => {
+    // const to = process.env.CRON_MAIL_USER;
 
-//     const formatDate = (date) => {
-//         return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
-//     };
+    // const formatDate = (date) => {
+    //     return `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+    // };
 
-//     const createEmailTemplate = (title, message, time) => {
-//         return `
-//                 <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
-//                     <h2 style="color: #007BFF; text-align: center;">${title}</h2>
-//                     <p style="font-size: 14px; text-align: center; color: #555;">${time}</p>
-//                     <hr style="margin: 20px 0; border-top: 1px solid #eee;">
-//                     <p style="font-size: 16px; margin-bottom: 20px;">${message}</p>
-//                     <footer style="text-align: center; font-size: 12px; color: #999;">
-//                         <p>Cron Task Notification</p>
-//                     </footer>
-//                 </div>
-//             `;
-//     };
+    // const createEmailTemplate = (title, message, time) => {
+    //     return `
+    //             <div style="font-family: Arial, sans-serif; line-height: 1.6; color: #333; max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px;">
+    //                 <h2 style="color: #007BFF; text-align: center;">${title}</h2>
+    //                 <p style="font-size: 14px; text-align: center; color: #555;">${time}</p>
+    //                 <hr style="margin: 20px 0; border-top: 1px solid #eee;">
+    //                 <p style="font-size: 16px; margin-bottom: 20px;">${message}</p>
+    //                 <footer style="text-align: center; font-size: 12px; color: #999;">
+    //                     <p>Cron Task Notification</p>
+    //                 </footer>
+    //             </div>
+    //         `;
+    // };
 
-//     const executeTask = async (retryCount = 0) => {
-//         try {
-//             const startTime = new Date();
-//             let startData = createEmailTemplate(
-//                 "Cron Started",
-//                 `Cron task started successfully.`,
-//                 `Time: ${formatDate(startTime)}`
-//             );
-//             await sendEmail({ title: "Cron Started", html: startData }, to, "Cron Started");
+    // const executeTask = async (retryCount = 0) => {
+    //     try {
+    //         const startTime = new Date();
+    //         let startData = createEmailTemplate(
+    //             "Cron Started",
+    //             `Cron task started successfully.`,
+    //             `Time: ${formatDate(startTime)}`
+    //         );
+    //         await sendEmail({ title: "Cron Started", html: startData }, to, "Cron Started");
 
-//             const response = await axios.post(`http://localhost:${port}/api/jobs`);
+    //         const response = await axios.post(`http://localhost:${port}/api/jobs`);
 
-//             if (response.status === 200 || response.status === 201) {
-//                 const endTime = new Date();
-//                 let endData = createEmailTemplate(
-//                     "Cron Ended",
-//                     `Cron task ended successfully. Message: ${response.data.message}`,
-//                     `Time: ${formatDate(endTime)}`
-//                 );
-//                 await sendEmail({ title: "Cron Ended", html: endData }, to, "Cron Ended");
-//             }
-//         } catch (error) {
-//             const errorTime = new Date();
-//             let errorData = createEmailTemplate(
-//                 "Error Occurred",
-//                 `An error occurred: ${error.message || error}. The function will retry.`,
-//                 `Time: ${formatDate(errorTime)}`
-//             );
-//             await sendEmail({ title: "Error Occurred", html: errorData }, to, "Cron Error Occured");
+    //         if (response.status === 200 || response.status === 201) {
+    //             const endTime = new Date();
+    //             let endData = createEmailTemplate(
+    //                 "Cron Ended",
+    //                 `Cron task ended successfully. Message: ${response.data.message}`,
+    //                 `Time: ${formatDate(endTime)}`
+    //             );
+    //             await sendEmail({ title: "Cron Ended", html: endData }, to, "Cron Ended");
+    //         }
+    //     } catch (error) {
+    //         const errorTime = new Date();
+    //         let errorData = createEmailTemplate(
+    //             "Error Occurred",
+    //             `An error occurred: ${error.message || error}. The function will retry.`,
+    //             `Time: ${formatDate(errorTime)}`
+    //         );
+    //         await sendEmail({ title: "Error Occurred", html: errorData }, to, "Cron Error Occured");
 
-//             if (retryCount < 3) {
-//                 await executeTask(retryCount + 1);
-//             } else {
-//                 await sendEmail({ title: "Error Occurred", html: errorData }, to, "Cron Error Occured");
-//             }
-//         }
-//     };
+    //         if (retryCount < 3) {
+    //             await executeTask(retryCount + 1);
+    //         } else {
+    //             await sendEmail({ title: "Error Occurred", html: errorData }, to, "Cron Error Occured");
+    //         }
+    //     }
+    // };
 
 
-//     await executeTask();
+    // await executeTask();
 // });
 
 app.use((req, res, next) => {
