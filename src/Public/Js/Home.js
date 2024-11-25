4df56a1855cab30e2eb7e883e276e5dfb806b6d9
Albert Haciverdiyev <126559426+alberthaciverdiyev1@users.noreply.github.com>
2024-11-25 12:29:53 +0400

    added no data to home page and fixed job page no data card

--- src/Public/Js/Home.js
+++ src/Public/Js/Home.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+    function noDataCard() {
+        return `<div class="flex items-center justify-center h-72 bg-white  border border-custom rounded-lg">
+                    <div class="flex flex-col items-center justify-center w-full max-w-xs mx-auto">
+                      <div class="w-20 h-20 mx-auto bg-orange-500 rounded-full shadow-sm flex justify-center items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="33" height="32" viewBox="0 0 33 32" fill="none">
+                          <g id="File Search">
+                            <path id="Vector" d="M19.9762 4V8C19.9762 8.61954 19.9762 8.92931 20.0274 9.18691C20.2379 10.2447 21.0648 11.0717 22.1226 11.2821C22.3802 11.3333 22.69 11.3333 23.3095 11.3333H27.3095M18.6429 19.3333L20.6429 21.3333M19.3095 28H13.9762C10.205 28 8.31934 28 7.14777 26.8284C5.9762 25.6569 5.9762 23.7712 5.9762 20V12C5.9762 8.22876 5.9762 6.34315 7.14777 5.17157C8.31934 4 10.205 4 13.9762 4H19.5812C20.7604 4 21.35 4 21.8711 4.23403C22.3922 4.46805 22.7839 4.90872 23.5674 5.79006L25.9624 8.48446C26.6284 9.23371 26.9614 9.60833 27.1355 10.0662C27.3095 10.524 27.3095 11.0253 27.3095 12.0277V20C27.3095 23.7712 27.3095 25.6569 26.138 26.8284C24.9664 28 23.0808 28 19.3095 28ZM19.3095 16.6667C19.3095 18.5076 17.8171 20 15.9762 20C14.1352 20 12.6429 18.5076 12.6429 16.6667C12.6429 14.8257 14.1352 13.3333 15.9762 13.3333C17.8171 13.3333 19.3095 14.8257 19.3095 16.6667Z" stroke="#FFFFFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                          </g>
+                        </svg>
+                      </div>
+                      <div>
+                        <h2 class="text-center text-black text-xl font-semibold leading-loose pb-2">There’s no product here</h2>
+                        <p class="text-center text-black text-base font-normal leading-relaxed pb-4">Try changing your filters to <br />see appointments</p>
+                      </div>
+                    </div>
+                  </div>`
+    }
 
     async function getJobs() {
         await axios.get('/api/jobs').then(res => {
@@ -67,20 +84,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                    <span class="bg-blue-100 text-blue-700 px-1 py-0.5 rounded-lg text-sm">${element.sourceUrl}</span>
                                 <h4 class="text-lg text-gray-600 font-bold">
                                   ${(
-                                        (+element.minSalary === +element.maxSalary && +element.minSalary !== null && +element.minSalary !== 0)
-                                            ? +element.minSalary +" "+ element.currencySign
+                                (+element.minSalary === +element.maxSalary && +element.minSalary !== null && +element.minSalary !== 0)
+                                    ? +element.minSalary + " " + element.currencySign
+                                    : (
+                                        (+element.minSalary !== null && +element.minSalary !== 0)
+                                            ? +element.minSalary + '-'
+                                            : ""
+                                    ) + (
+                                        (+element.maxSalary !== null && +element.maxSalary !== 0)
+                                            ? +element.maxSalary + " " + element.currencySign
                                             : (
-                                                (+element.minSalary !== null && +element.minSalary !== 0)
-                                                    ? +element.minSalary + '-'
-                                                    : ""
-                                            ) + (
-                                                (+element.maxSalary !== null && +element.maxSalary !== 0)
-                                                    ? +element.maxSalary  +" "+ element.currencySign
-                                                    : (
-                                                        !element.minSalary && !element.maxSalary ? "Razılaşma ilə" : ""
-                                                    )
+                                                !element.minSalary && !element.maxSalary ? "Razılaşma ilə" : ""
                                             )
-                                    )} 
+                                    )
+                            )} 
                                     </h4>
                                 </div>
                             </div>
@@ -88,20 +105,20 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                 <div class="text-right">
                             <h4 class="text-lg text-gray-600 font-bold mt-2">
                                 ${(
-                                        (+element.minSalary === +element.maxSalary && +element.minSalary !== null && +element.minSalary !== 0) 
-                                        ? +element.minSalary 
-                                        : (
-                                            (+element.minSalary !== null && +element.minSalary !== 0) 
-                                            ? +element.minSalary + '-' 
+                                (+element.minSalary === +element.maxSalary && +element.minSalary !== null && +element.minSalary !== 0)
+                                    ? +element.minSalary
+                                    : (
+                                        (+element.minSalary !== null && +element.minSalary !== 0)
+                                            ? +element.minSalary + '-'
                                             : ""
-                                        ) + (
-                                            (+element.maxSalary !== null && +element.maxSalary !== 0) 
-                                            ? +element.maxSalary 
+                                    ) + (
+                                        (+element.maxSalary !== null && +element.maxSalary !== 0)
+                                            ? +element.maxSalary
                                             : (
                                                 !element.minSalary && !element.maxSalary ? "Razılaşma ilə" : ""
                                             )
-                                        )
-                                    )}                                                  
+                                    )
+                            )}                                                  
                             </h4>
                                 </div>
                                 <div class="flex justify-end items-end mt-auto pt-16">
@@ -114,9 +131,11 @@ document.addEventListener("DOMContentLoaded", (event) => {
                     </div> `;
                     });
 
+                    document.getElementById("home-card-section").innerHTML = htmlContent;
+                } else {
+                    document.getElementById("card-section").innerHTML = noDataCard();
                 }
 
-                document.getElementById("home-card-section").innerHTML = htmlContent;
 
                 const jobCards = document.querySelectorAll('.job-card');
                 jobCards.forEach(card => {
@@ -179,12 +198,12 @@ document.addEventListener("DOMContentLoaded", (event) => {
         const categorySelect = document.getElementById('category-select');
         const citySelect = document.getElementById('city-select');
         const keywordInput = document.getElementById('keyword');
-    
+
         const categoryId = categorySelect?.value || 'all';
         const cityId = citySelect?.value || 'all';
         const keyword = keywordInput?.value?.trim().toLowerCase() || '';
-    
-        const baseUrl = `${window.location.origin}/jobs`; 
+
+        const baseUrl = `${window.location.origin}/jobs`;
         const params = new URLSearchParams({
             minSalary: 0,
             maxSalary: 5000,
@@ -197,5 +216,5 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
         window.location.href = `${baseUrl}?${params.toString()}`;
     });
-    
+
 });
