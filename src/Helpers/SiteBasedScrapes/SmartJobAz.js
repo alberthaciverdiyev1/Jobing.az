6e020f5b8699aa52a9fe2f3bca668b942b56f741
Albert Haciverdiyev <126559426+alberthaciverdiyev1@users.noreply.github.com>
2024-11-28 12:28:20 +0400

    added timeout to smartjob.az scrape

--- src/Helpers/SiteBasedScrapes/SmartJobAz.js
+++ src/Helpers/SiteBasedScrapes/SmartJobAz.js
@@ -57,104 +57,112 @@ class SmartJobAz {
     }
 
     async Jobs(categories, bossAzcities) {
-        let $ = await Scrape(`https://${this.url}`);
-        const token = $('input[name="_token"]').val();
-
+        const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
         try {
+            let $ = await Scrape(`https://${this.url}`);
+            const token = $('input[name="_token"]').val();
+    
             const filteredCategories = categories.filter(c => c.website === enums.SitesWithId.SmartJobAz);
             const limit = pLimit(3);
             const dataPromises = [];
             const educationIds = [1, 2, 5, 6, 7, 9, 10, 11, 12, 13, 0];
-            const filteredJobs = [];
-            const seenUrls = new Set();
             const jobData = [];
             const companyData = [];
+    
             for (const category of filteredCategories) {
                 for (const education of educationIds) {
                     for (let page = 0; page <= 2; page++) {
                         const requestPromise = limit(async () => {
                             try {
                                 const url = `https://${this.url}/vacancies?_token=${encodeURIComponent(token)}&job_category_id%5B20%5D=${encodeURIComponent(category.categoryId)}&education_id%5B0%5D=${encodeURIComponent(education)}&salary_from=&salary_to=&page=${page}`;
-                                const $ = await Scrape(url);
-
-                                $('.brows-job-list').each((i, el) => {
-                                    const urlAndId = $(el).find('h3 a');
-                                    const title = urlAndId.text().trim();
-                                    const companyElement = $(el).find('.company-title a');
-                                    const companyName = companyElement.text().trim();
-                                    const companyId = companyElement.attr('href')?.split('/').pop() || null;
-                                    const location = $(el).find('.location-pin').text().trim();
-                                    const salaryText = $(el).find('.salary-val').text().trim();
-                                    const companyImageUrl = $(el).find('.brows-job-company-img img').attr('src');
-                                    const cleanSalaryText = salaryText.replace('AZN', '').trim();
-                                    const parts = cleanSalaryText.split(' - ');
-                                    const jobId = urlAndId.attr('href')?.split('/').pop() || null;
-                                    const redirectUrl = urlAndId.attr('href') || null;
-                                    let [minSalary, maxSalary] = [null, null];
-                                    if (parts.length === 2) {
-                                        minSalary = parseInt(parts[0], 10);
-                                        maxSalary = parseInt(parts[1], 10);
-                                    } else if (parts.length === 1) {
-                                        minSalary = maxSalary = parseInt(parts[0], 10);
-                                    }
-
-                                    const locationCity = bossAzcities.find(x => x.name === location);
-
-                                    jobData.push({
-                                        title,
-                                        companyName,
-                                        companyId,
-                                        minSalary,
-                                        maxSalary,
-                                        location,
-                                        cityId: locationCity ? +locationCity.cityId : null,
-                                        description: null,
-                                        jobId,
-                                        categoryId: category.localCategoryId || null,
-                                        sourceUrl: this.url,
-                                        redirectUrl,
-                                        jobType: '0x001',
-                                        educationId: this.mapEducation(education),
-                                        experienceId: null,
-                                        uniqueKey: `${title}-${companyName}-${location}`
-                                    });
-
-                                    companyData.push({
-                                        companyName,
-                                        imageUrl: companyImageUrl,
-                                        website: enums.SitesWithId.SmartJobAz,
-                                        uniqueKey: `${companyName}-${companyImageUrl}`
-                                    });
-                                    // console.log({companyData});
-
+    
+                                const timeout = new Promise((_, reject) => {
+                                    setTimeout(() => reject(new Error('Request timed out')), 30000);
                                 });
-
+    
+                                const fetchPromise = (async () => {
+                                    const $ = await Scrape(url);
+                                    $('.brows-job-list').each((i, el) => {
+                                        const urlAndId = $(el).find('h3 a');
+                                        const title = urlAndId.text().trim();
+                                        const companyElement = $(el).find('.company-title a');
+                                        const companyName = companyElement.text().trim();
+                                        const companyId = companyElement.attr('href')?.split('/').pop() || null;
+                                        const location = $(el).find('.location-pin').text().trim();
+                                        const salaryText = $(el).find('.salary-val').text().trim();
+                                        const companyImageUrl = $(el).find('.brows-job-company-img img').attr('src');
+                                        const cleanSalaryText = salaryText.replace('AZN', '').trim();
+                                        const parts = cleanSalaryText.split(' - ');
+                                        const jobId = urlAndId.attr('href')?.split('/').pop() || null;
+                                        const redirectUrl = urlAndId.attr('href') || null;
+                                        let [minSalary, maxSalary] = [null, null];
+                                        if (parts.length === 2) {
+                                            minSalary = parseInt(parts[0], 10);
+                                            maxSalary = parseInt(parts[1], 10);
+                                        } else if (parts.length === 1) {
+                                            minSalary = maxSalary = parseInt(parts[0], 10);
+                                        }
+    
+                                        const locationCity = bossAzcities.find(x => x.name === location);
+    
+                                        jobData.push({
+                                            title,
+                                            companyName,
+                                            companyId,
+                                            minSalary,
+                                            maxSalary,
+                                            location,
+                                            cityId: locationCity ? +locationCity.cityId : null,
+                                            description: null,
+                                            jobId,
+                                            categoryId: category.localCategoryId || null,
+                                            sourceUrl: this.url,
+                                            redirectUrl,
+                                            jobType: '0x001',
+                                            educationId: this.mapEducation(education),
+                                            experienceId: null,
+                                            uniqueKey: `${title}-${companyName}-${location}`
+                                        });
+    
+                                        companyData.push({
+                                            companyName,
+                                            imageUrl: companyImageUrl,
+                                            website: enums.SitesWithId.SmartJobAz,
+                                            uniqueKey: `${companyName}-${companyImageUrl}`
+                                        });
+                                    });
+                                })();
+    
+                                await Promise.race([fetchPromise, timeout]);
+    
                             } catch (error) {
                                 console.error(`Error fetching data from ${url}:`, error.message);
                                 return [];
                             }
                         });
-
+    
                         dataPromises.push(requestPromise);
                     }
                 }
             }
-
+    
             const results = await Promise.all(dataPromises);
-            results.flat();
+            results.flat(); 
+    
             const companyResult = await CompanyService.create(companyData);
             if (companyResult.status === 200 || companyResult.status === 201) {
                 return jobData;
-
+            } else {
+                console.error('Failed to save company data:', companyResult);
+                throw new Error('Failed to save company data');
             }
-
-
+    
         } catch (error) {
             console.error('Error fetching jobs:', error.message, error.stack);
             throw new Error('Error fetching jobs');
         }
     }
-
+    
     mapEducation(education) {
         return (education === 10 || education === 2 || education === 13 || education === 6) ? enums.Education.Secondary :
             (education === 0 || education === 7) ? enums.Education.IncompleteEducation :
