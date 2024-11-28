796f77f821555975e0656fdee3506dbe0709e8b3
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-11-28 09:03:39 -0800

    fixed length and isNan parse error

--- src/Helpers/SiteBasedScrapes/OfferAz.js
+++ src/Helpers/SiteBasedScrapes/OfferAz.js
@@ -102,10 +102,10 @@ class OfferAz {
     
                                     let [minSalary, maxSalary] = [0, 0];
                                     if (parts.length === 2) {
-                                        minSalary = parseInt(parts[0], 10);
-                                        maxSalary = parseInt(parts[1], 10);
+                                        minSalary = !isNaN(Number(parts[0])) ? parseInt(parts[0], 10) : 0;
+                                        maxSalary = !isNaN(Number(parts[1])) ? parseInt(parts[1], 10) : 0;
                                     } else if (parts.length === 1) {
-                                        minSalary = maxSalary = parseInt(parts[0], 10);
+                                        minSalary = maxSalary = !isNaN(Number(parts[0])) ? parseInt(parts[0], 10) : 0;
                                     }
     
                                     jobData.push({
