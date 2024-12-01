fa0ca88bac882af29394189071d2182ffd35b877
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-12-01 07:08:00 -0800

    test real ip

--- src/Middlewares/Visitors.js
+++ src/Middlewares/Visitors.js
@@ -2,7 +2,7 @@ import VisitorService from "../Services/VisitorService.js";
 
 const visitorLogger = async (req, res, next) => {
     try {
-        const ipAddress = req.ip;
+        const ipAddress = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for']?.split(',')[0] || req.ip;
         const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
 
         let visitor = await VisitorService.findByIp(ipAddress);
