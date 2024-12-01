a1fccd008cc1b1f3640fe007a2d7bc73c0585ddd
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-12-01 07:34:38 -0800

    setup visitor middleware for spesific routes

--- src/Models/Visitor.js
+++ src/Models/Visitor.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const visitorSchema = new Schema({
-    ip: {
+    visitIp: {
         type: String,
         required: true,
         unique: true
