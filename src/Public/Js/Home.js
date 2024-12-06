a4ec91949ccf3b15612dc985467ce8ff1dad35b7
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-12-06 08:33:10 -0800

    updated home page front

--- src/Public/Js/Home.js
+++ src/Public/Js/Home.js
@@ -29,25 +29,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 if (res.data.totalCount) {
                     let data = res.data.jobs.slice(0, 12);
                     data.forEach(element => {
-                        htmlContent += `<div class="job-card bg-white px-3 pt-2 h-36 sm:h-40 rounded-xl shadow-md hover:hover-card-color cursor-pointer duration-300 border border-custom sm:px-5" data-original-link="${element.redirectUrl}">
+                        htmlContent += `<div class="job-card bg-white px-3 pt-2 h-36 lg:h-40 rounded-xl shadow-md hover:hover-card-color cursor-pointer duration-300 border border-custom lg:px-5" data-original-link="${element.redirectUrl}">
                         <div class="content flex">
-                             <div class="mt-2 flex-shrink-0 sm:mt-1">
-                                <img src="../Images/${element.sourceUrl}.png" alt="Company Logo" class="border-custom h-12 w-12 mt-1 rounded-lg border sm:h-14 sm:w-14" />
-                                <img src="${(element.companyImageUrl && element.companyImageUrl !== "/nologo.png" && !element.companyImageUrl.startsWith('http')) ? element.companyImageUrl.replace(/src\/Public/g, '..') : (element.companyImageUrl && element.companyImageUrl.startsWith('http') ? element.companyImageUrl : "../Images/DefaultCompany.png")}" alt="Company Logo" class="border-custom h-12 w-12 mt-3 rounded-lg border sm:h-14 sm:w-14" />
+                             <div class="mt-2 flex-shrink-0 lg:mt-1">
+                                <img src="../Images/${element.sourceUrl}.png" alt="Company Logo" class="border-custom h-12 w-12 mt-1 rounded-lg border lg:h-14 lg:w-14" />
+                                <img src="${(element.companyImageUrl && element.companyImageUrl !== "/nologo.png" && !element.companyImageUrl.startsWith('http')) ? element.companyImageUrl.replace(/src\/Public/g, '..') : (element.companyImageUrl && element.companyImageUrl.startsWith('http') ? element.companyImageUrl : "../Images/DefaultCompany.png")}" alt="Company Logo" class="border-custom h-12 w-12 mt-3 rounded-lg border lg:h-14 lg:w-14" />
                             </div>
-                            <div class="ml-3 mt-2 pr-1 sm:mt-2 justify-end w-screen">
+                            <div class="ml-3 mt-2 pr-1 lg:mt-2 justify-end w-screen">
                                 <div class="flex mb-1 justify-between">
                                     <div class="w-full">
                                         <div class="flex justify-between w-full">
-                                                <p class="text-sm font-bold flex text-gray-700 justify-items-start sm:font-bold sm:text-base mb-1">
-                                                    <span class="truncate sm:hidden"> 
+                                                <p class="text-sm font-bold flex text-gray-700 justify-items-start lg:font-bold lg:text-base mb-1">
+                                                    <span class="truncate lg:hidden"> 
                                                         ${capitalizeFirstLetter(element.title.slice(0, 17)) + (element.title.length > 17 ? "..." : "")} 
                                                     </span>
-                                                    <span class="hidden sm:inline sm:whitespace-normal"> 
+                                                    <span class="hidden lg:inline lg:whitespace-normal"> 
                                                         ${capitalizeFirstLetter(element.title.slice(0, 25)) + (element.title.length > 25 ? "..." : "")} 
                                                     </span>
                                                 </p>
-                                            <h4 class=" text-gray-600 hidden sm:block ${!element.minSalary && !element.maxSalary ? "font-semibold text-base" :"font-bold text-lg"}">
+                                            <h4 class=" text-gray-600 hidden lg:block ${!element.minSalary && !element.maxSalary ? "font-semibold text-base" :"font-bold text-lg"}">
                                                 ${(
                                             (+element.minSalary === +element.maxSalary && +element.minSalary !== null && +element.minSalary !== 0)
                                                 ? +element.minSalary + " " + element.currencySign
@@ -65,14 +65,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                                 )}                                                
                                             </h4>
                                         </div>
-                                        <h4 class="truncate sm:hidden text-sm font-semibold text-gray-700 mb-1"> 
+                                        <h4 class="truncate lg:hidden text-sm font-semibold text-gray-700 mb-1"> 
                                             <i class="fa-solid fa-building"></i> ${capitalizeFirstLetter(element.companyName.slice(0, 17)) + (element.companyName.length > 17 ? "..." : "")}
                                         </h4>
-                                        <h4 class="hidden sm:inline sm:whitespace-normal text-sm font-semibold text-gray-700 mb-1"> 
+                                        <h4 class="hidden lg:inline lg:whitespace-normal text-sm font-semibold text-gray-700 mb-1"> 
                                             <i class="fa-solid fa-building"></i> ${capitalizeFirstLetter(element.companyName.slice(0, 32)) + (element.companyName.length > 32 ? "..." : "")}
                                         </h4>
                                     </div>
-                                    <div class="hidden sm:w-full">
+                                    <div class="hidden lg:w-full">
                                         <span class="bg-yellow-100 text-yellow-700 px-1 ml-2 py-0.5 rounded-lg text-sm w-auto">
                                             ${element.sourceUrl}
                                         </span>
@@ -82,9 +82,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                     <span><i class="fa-solid fa-clock mr-0.5"></i> ${element.postedAt.slice(0, 10)}</span>
                                     <span class="ml-3"><i class="fa-solid fa-location-dot mr-0.5"></i> ${element.location.slice(0, 10) + (element.location.length > 10 ? "..." : "")}</span>
                                 </div>
-                                <div class="border-custom-top w-52 mt-2 sm:w-72"></div>
-                                    <div class="text-sm mt-2 hidden sm:flex items-center justify-between">
-                                      <div class="flex">  <span class="bg-yellow-100 text-yellow-700 px-2 py-0.5 font-medium rounded-lg text-sm h-7 hidden sm:flex">
+                                <div class="border-custom-top w-52 mt-2 lg:w-72"></div>
+                                    <div class="text-sm mt-2 hidden lg:flex items-center justify-between">
+                                      <div class="flex">  <span class="bg-yellow-100 text-yellow-700 px-2 py-0.5 font-medium rounded-lg text-sm h-7 hidden lg:flex">
                                             ${element.sourceUrl}
                                         </span>
                                          ${element.isPremium ? `<span class="bg-orange-500 text-white px-2 ml-1 py-0.5 rounded-lg">premium</span>` : ''}
@@ -95,7 +95,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                     </a>
                                 </div>
                                         </div>
-                                <div class="text-sm mt-2 flex justify-between sm:hidden">
+                                <div class="text-sm mt-2 flex justify-between lg:hidden">
                                    <span class="bg-blue-100 text-blue-700 px-1 py-0.5 rounded-lg text-sm">${element.sourceUrl}</span>
                                 <h4 class="text-gray-600 ${!element.minSalary && !element.maxSalary ? "font-semibold text-base" :"font-bold text-lg"}">
                                    ${(
@@ -116,7 +116,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
                                  </h4>
                                 </div>
                             </div>
-                            <div class="flex flex-col justify-between h-full flex-grow hidden sm:flex">
+                            <div class="flex flex-col justify-between h-full flex-grow hidden lg:flex">
 
 
                             </div>
