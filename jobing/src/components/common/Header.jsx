4d68a768ae8436a57b998d747633dab9da608eda
Asad Babayev <esedbabayev12@gmail.com>
2025-05-05 03:12:59 +0400

    add classses

--- jobing/src/components/common/Header.jsx
+++ jobing/src/components/common/Header.jsx
@@ -11,12 +11,16 @@ import Logo from "../../images/Jobing.az-Logo.png";
 
 const Header = () => {
   return (
-    <header className="w-full py-5 px-12">
+    <header className="w-full py-2 px-4 sticky">
       <Container>
-        <div className="flex">
-          <div>
+        <div className="flex justify-between items-center h-20">
+          <div className="w-48">
             <Link to="/">
-              <img src={Logo} alt="logo" />
+              <img
+                src={Logo}
+                alt="logo"
+                className="w-auto object-contain"
+              />
             </Link>
           </div>
           <NavBar />
