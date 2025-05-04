a853b1a8e376d87eb11f33d86056713e23501f09
Asad Babayev <esedbabayev12@gmail.com>
2025-05-04 23:34:56 +0400

    add routes

--- jobing/src/App.jsx
+++ jobing/src/App.jsx
@@ -1,12 +1,22 @@
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 // Container
-import Container from './components/common/Container';
+import Container from "./components/common/Container";
+
+// Pages
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <Container>
-    
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
     </Container>
   );
 }
