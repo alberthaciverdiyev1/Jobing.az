6a1176f8ed98c08e9ad6dd0876d0488d57147423
Asad Babayev <esedbabayev12@gmail.com>
2025-05-04 23:35:57 +0400

    make updates

--- jobing/src/components/common/Header.jsx
+++ jobing/src/components/common/Header.jsx
@@ -2,17 +2,26 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 
+// Components
+import Container from "./Container";
+import NavBar from "./NavBar";
+
 // Images
-import Logo from "../../images/Jobing.az-Logo.png"
+import Logo from "../../images/Jobing.az-Logo.png";
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <Link to="/">
-            <img src={Logo} alt="logo" />
-        </Link>
-      </div>
+    <header className="w-full py-5 px-12">
+      <Container>
+        <div className="flex">
+          <div>
+            <Link to="/">
+              <img src={Logo} alt="logo" />
+            </Link>
+          </div>
+          <NavBar />
+        </div>
+      </Container>
     </header>
   );
 };
