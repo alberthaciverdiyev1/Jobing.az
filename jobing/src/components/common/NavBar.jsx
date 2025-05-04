2120631dc6ddfe086960880b5095459a566a420d
Asad Babayev <esedbabayev12@gmail.com>
2025-05-05 03:13:28 +0400

    add Navlinks

--- jobing/src/components/common/NavBar.jsx
+++ jobing/src/components/common/NavBar.jsx
@@ -1,14 +1,47 @@
 import React from "react";
-
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const linkClasses = ({ isActive }) =>
+    `block px-4 py-2 text-sm font-medium rounded cursor-pointer ${
+      isActive
+        ? "bg-[#fe8012] text-white"
+        : "text-[#202a38] hover:bg-[#fe8012] hover:text-white"
+    }`;
+
   return (
-    <nav>
-      <ul>
-        <Link to="/">
-          <li className="bg-orange-500">Ana Səhifə</li>
-        </Link>
+    <nav className="flex items-center justify-center">
+      <ul className="flex gap-8">
+        <li>
+          <NavLink to="/" end className={linkClasses}>
+            Ana Səhifə
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/vakansiyalar" className={linkClasses}>
+            Vakansiyalar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/bloqlar" className={linkClasses}>
+            Bloqlar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/elaqe" className={linkClasses}>
+            Əlaqə
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/haqqımızda" className={linkClasses}>
+            Haqqımızda
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/elan" className={linkClasses}>
+            Elan Yerləşdir
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
