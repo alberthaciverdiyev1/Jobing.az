e86dac861656ad4d70f04d933096b360ac0cd580
Asad Babayev <esedbabayev12@gmail.com>
2025-05-07 02:55:33 +0400

    change z-index

--- jobing/src/components/common/Header.jsx
+++ jobing/src/components/common/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "../../components/common/Logo";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-20 w-full pr-4 shadow-[0_4px_4px_-4px_rgba(0,0,0,0.4)]">
+    <header className="sticky top-0 z-50 w-full pr-4 shadow-[0_4px_4px_-4px_rgba(0,0,0,0.4)]">
       <Container>
         <div className="flex items-center justify-between h-20">
           <Logo />
