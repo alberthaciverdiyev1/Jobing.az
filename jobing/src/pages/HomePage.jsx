532bda2d45176be0e86cd3363ed09ef8744f0dcb
Asad Babayev <esedbabayev12@gmail.com>
2025-05-07 02:56:02 +0400

    add hero section

--- jobing/src/pages/HomePage.jsx
+++ jobing/src/pages/HomePage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 // Components
-
+import HeroSection from "../components/HomePage/HeroSection";
 
 const HomePage = () => {
   return (
-    <>
-      
-    </>
-  )
-}
+    <main>
+      <HeroSection />
+    </main>
+  );
+};
 
-export default HomePage
+export default HomePage;
