5f51124fb9e514f15a73c06372484effae0fcade
HuseyNN021 <hhsnov880@gamil.com>
2025-08-16 07:47:37 -0700

    dil secimi, light/dark buttonu, cardlarda is sayti img silindi, filter elave olundu

--- src/Public/Js/Details.js
+++ src/Public/Js/Details.js
@@ -15,10 +15,9 @@ document.addEventListener('DOMContentLoaded', async () => {
                     let data = res.data.jobs.slice(0, 12);
                     data.forEach(element => {
                         htmlContent += `<div class="job-card bg-white px-3 pt-2 h-36 lg:h-40 rounded-xl shadow-md hover:hover-card-color cursor-pointer duration-300 border border-custom lg:px-5" data-original-link="${element.redirectUrl}">
-                            <div class="content flex">
+                            <div class="content flex items-center">
                                  <div class="mt-2 flex-shrink-0 lg:mt-1">
-                                    <img src="../../Images/${element.sourceUrl}.png" alt="Company Logo" class="border-custom h-12 w-12 mt-1 rounded-lg border lg:h-14 lg:w-14" />
-                                <img src="${(element.companyImageUrl && element.companyImageUrl !== "/nologo.png" && !element.companyImageUrl.startsWith('http')) ? element.companyImageUrl.replace(/src\/Public/g, '../..') : (element.companyImageUrl && element.companyImageUrl.startsWith('http') ? element.companyImageUrl : "../../Images/DefaultCompany.png")}" alt="Company Logo" class="border-custom h-12 w-12 mt-3 rounded-lg border lg:h-14 lg:w-14" />
+                                <img src="../../Images/${element.sourceUrl}.png" alt="Company Logo" class="w-[98.57px] h-[98.57px] rotate-0 opacity-100 rounded-[8.57px] border" />
                         </div>
                                 <div class="ml-3 mt-2 pr-1 lg:mt-2 justify-end w-screen">
                                     <div class="flex mb-1 justify-between">
