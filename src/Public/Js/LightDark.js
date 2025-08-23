2cf52e16230e283e87bcc60faa7f4bc738d41b68
HuseyNN021 <hhsnov880@gamil.com>
2025-08-23 08:53:53 -0700

    update responsive

--- src/Public/Js/LightDark.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById("lightButton").onclick = function() {
-        const nav = document.getElementsByTagName("nav")[0]; // ilk <nav> elementi
-        nav.style.backgroundColor = "black"; // fon rəngi
-        nav.style.color = "white"; // mətn rəngi
-    }
-});
