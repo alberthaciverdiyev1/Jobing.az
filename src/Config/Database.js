e08f9744d938848471c7cfbb6690b85e3439ac2e
alberthaciveridyev1 <alberthaciverdiyev55@gmail.com>
2026-05-13 01:11:07 -0700

    fixed rss feed

--- src/Config/Database.js
+++ src/Config/Database.js
@@ -9,11 +9,14 @@ dns.setServers(['192.168.31.1', '8.8.8.8', '1.1.1.1']);
 
 const dbURI = process.env.NODE_ENV !== "production" ? `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}` : process.env.REMOTE_DB_URL;
 
-mongoose.connect(dbURI, {
+const connectPromise = mongoose.connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-.then(() => console.log('Successfully connected to the database.'))
-.catch(err => console.error('Connection error:', err));
+});
 
+connectPromise
+    .then(() => console.log('Successfully connected to the database.'))
+    .catch(err => console.error('Connection error:', err));
+
+export { connectPromise };
 export default mongoose;
