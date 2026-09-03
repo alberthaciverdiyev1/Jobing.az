704d22a7bdf31336546434042dea38f9aa41ee0c
albert <alberthaciverdiyev55@gmail.com>
2026-09-03 16:51:58 +0400

    Ensure company message template list shows 'add' button
    - Explicit CreateAction ('Şablon Əlavə Et') on company template list header
    - CompanyMessageTemplateResource canCreate returns true

--- app/Modules/Company/Filament/Resources/CompanyMessageTemplateResource/Pages/ListCompanyMessageTemplates.php
+++ app/Modules/Company/Filament/Resources/CompanyMessageTemplateResource/Pages/ListCompanyMessageTemplates.php
@@ -3,9 +3,17 @@
 namespace App\Modules\Company\Filament\Resources\CompanyMessageTemplateResource\Pages;
 
 use App\Modules\Company\Filament\Resources\CompanyMessageTemplateResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCompanyMessageTemplates extends ListRecords
 {
     protected static string $resource = CompanyMessageTemplateResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()->label('Şablon Əlavə Et'),
+        ];
+    }
 }
