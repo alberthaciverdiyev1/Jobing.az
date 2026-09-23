784e4c817a11f57868016edf58648e0c2d3a338f
albert <alberthaciverdiyev55@gmail.com>
2026-09-23 23:02:07 +0400

    Set explicit table name for ScraperSourceStatus model

--- app/Modules/Scraper/Models/ScraperSourceStatus.php
+++ app/Modules/Scraper/Models/ScraperSourceStatus.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ScraperSourceStatus extends Model
 {
+    protected $table = 'scraper_source_status';
     protected $guarded = ['id'];
     protected $casts = ['last_run_at' => 'datetime'];
 }
