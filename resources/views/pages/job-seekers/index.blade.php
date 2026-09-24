b06b4a2778e45174c6c2f3cb4adfb95767fc4e15
albert <alberthaciverdiyev55@gmail.com>
2026-09-24 14:56:43 +0400

    Fix unescaped apostrophe in Turkish hero string

--- resources/views/pages/job-seekers/index.blade.php
+++ resources/views/pages/job-seekers/index.blade.php
@@ -67,7 +67,7 @@ window.__JOB_SEEKERS_CONFIG__ = {
                     {{ __('Job Seekers') }}
                 </h1>
                 <p class="text-xs sm:text-sm md:text-base text-gray-500">
-                    {{ __('Kuzey Kıbrıs'ın önde gelen şirketlerinde binlerce aktif iş ilanı ve kariyer imkanı') }}
+                    {{ __('Kuzey Kıbrıs\'ın önde gelen şirketlerinde binlerce aktif iş ilanı ve kariyer imkanı') }}
                 </p>
             </div>
 
